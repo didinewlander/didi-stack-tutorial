87f0705b862123e7d1d76aafc2bc74393212ca81
Yedidya Newlander <ydidya.n@gmail.com>
2024-09-12 16:04:05 +0300

    tutorial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,117 @@
+import { AnimatePresence, motion } from "framer-motion"
+import { Loader2 } from "lucide-react"
+import { useEffect, useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+import { TypeAnimation } from "react-type-animation"
+import { Button } from "./components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./components/ui/card"
+import { Textarea } from "./components/ui/textarea"
 
-    import './App.css';
-    
-    const docsLinks = [
-      { name: 'Vite', url: 'https://vitejs.dev' },
-      { name: 'React', url: 'https://react.dev' },
-      { name: 'TailwindCSS', url: 'https://tailwindcss.com/docs' },
-      { name: 'Shadcn/UI', url: 'https://ui.shadcn.com/' },
-    ];
-    
-    function App() {
-      return (
-        <div className="min-h-screen py-8 px-4 flex flex-col items-center">
-          <h1 className="text-4xl font-bold text-gray-800 mb-8">Didi-Stack UI Starter kit</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {docsLinks.map((doc) => (
-              <a
-                key={doc.name}
-                href={doc.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-slate-200 p-6 shadow-lg rounded-lg transform transition hover:scale-105 hover:shadow-2xl"
-              >
-                <h2 className="text-xl font-semibold text-center text-gray-700">{doc.name}</h2>
-              </a>
-            ))}
-          </div>
-          <footer className="mt-12 text-gray-600">
-            <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
-          </footer>
-        </div>
-      );
+const chatStateTexts = {
+  idle: 'I\'m ready when you are',
+  typing: 'Typing...',
+  processing: 'Let me think about it',
+  response: 'Responding'
+}
+
+function App() {
+  const [input, setInput] = useState<string>('');
+  const [response, setResponse] = useState<string>('');
+  const [chatState, setChatState] = useState<"idle" | "typing" | "processing" | "response">("idle");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+    setChatState("typing");
+  }
+  const getResponse = async () => {
+    setIsLoading(true);
+    if (input.trim() === '') {
+      toast('Please enter text message');
     }
-    
-    export default App;
-    
+    else {
+      setResponse(input);
+      setChatState("processing")
+      // here perform api call to AI model
+      await new Promise(resolve => setTimeout(resolve, Math.min(input.length * 50, 3000)))
+      setInput('');
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (chatState === "typing") {
+        setChatState("idle");
+      }
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [input, chatState]);
+  useEffect(() => {
+    if (response) {
+      setChatState("response");
+    }
+  }, [response])
+
+  return (
+    <div className="min-h-screen bg-slate-200 flex flex-col items-center justify-center p-4">
+      <Toaster />
+      <AnimatePresence>
+        <Card className="w-full max-w-md mt-4">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl">
+              Squid Master
+            </CardTitle>
+            <CardDescription className="py-4">
+              Squid Master will help you handle evereything all at once.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="text-center font-bold">
+              {chatStateTexts[chatState]}
+            </div>
+            <Textarea className="min-h-[60px]"
+              value={input}
+              onChange={handleInputChange}
+
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  getResponse();
+                }
+              }}
+              placeholder="Please write a description of your problem..."
+            />
+            <Button className="w-full" onClick={getResponse} disabled={isLoading}>
+              {isLoading ? <Loader2 className="animate-spin "/> :"Send your request to Squid Master"}
+            </Button>
+            {response && (
+              <div className="border-2 border-slate-200 rounded-lg px-4 py-2 pb-6">
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={response}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.5 }}
+                  >
+                    <TypeAnimation
+                      sequence={[response, () => setChatState("idle")]}
+                      wrapper="p"
+                      speed={50}
+                      className="italic mt-4 text-slate-400 break-words"
+                    />
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+            )}
+
+          </CardContent>
+        </Card>
+      </AnimatePresence>
+    </div>
+  )
+}
+
+
+export default App
